98bd436ed43c184ee7e2efe8eebf2ff8fd9952b9
Mztrem7 <mamacogodzilla7@gmail.com>
2023-06-22 11:25:04 -0300

    Terminando a lógica

---
+++ script.js
@@ -0,0 +1,7 @@
+// declaração da variável
+let input = document.getElementById("input");
+
+// função de mostrar o texto quando o botão for acionado
+function mostra(){
+    alert("Seu nome é " + input.value)
+}
